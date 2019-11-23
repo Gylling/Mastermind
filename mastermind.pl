83a8cbaa3597e67ee4b54b9162ef825676cb598c
Erik Gylling <gylling.erik@gmail.com>
2019-11-23 13:51:21 -0800

    Lørdag

--- mastermind.pl
+++ mastermind.pl
@@ -1,5 +1,56 @@
 % Mastermind
 
+
+%Reads one char
+readOneChar(I) :- readln([I|_]).
+
+% init number of colors
+% C: Number of colors (output)
+initColor(C) :- write('Enter number of colors (min. 2, max. 10) '), readOneChar(A), checkColor(A,C).
+
+% check if color is out of bounds
+% A: Number of colors (input), C: Number of colors (output)
+checkColor(A,C) :- 1 < A, A < 11, C is A.
+checkColor(A,C) :- 2 > A, initColor(C).
+checkColor(A,C) :- A > 10, initColor(C).
+
+% init size of code
+% S: Size of code (output)
+initCodeSize(S) :- write('Enter size of code (min. 2, max. 10) '), readOneChar(A), checkSize(A,S).
+
+% check if size of code is out of bounds
+% A: Size of code (input), S: Size of code (output)
+checkSize(A,S) :- 1 < A, A < 11, S is A.
+checkSize(A,S) :- 2 > A, initCodeSize(S).
+checkSize(A,S) :- A > 10, initCodeSize(S).
+ 
+
+% Translate S colors to integers and checks if the line has S number of colors.
+% [H|T]: Code in colors, C: Code in integers, Cnt: Counter starting from S going to 0, S: size of code
+colToInt([], []).
+colToInt([H|T], [I|R]) :- color(I,H),colToInt(T, R). 
+
+% Parse line from colors to integers. 
+% S: Size of code, C: Code in integers
+parseLine(S,Co, C):-write('Enter your guess: '), readln(Ln), checkLine(Ln,S,Co, CLn), colToInt(CLn,C).
+
+% Checks if line has S colors.
+% Ln: List of colors, S: Number of colors in code, CLn: Correct input (Output).
+checkLine(Ln, S, Co, Ln ) :- length(Ln, S), checkColors(Ln,Co).
+checkLine(Ln, S, Co, CLn ) :- length(Ln, S), \+ checkColors(Ln,Co), write('Colors could not be recognized.'),nl,write('This is your coloroptions.'),showColoroptions(Co), write('Enter your new guess: '), readln(Ln1), checkLine(Ln1,S, CLn).
+checkLine(Ln, S, _, CLn) :- length(Ln, D), dif(S,D), write('You need to provide '),write(S),write(' colors.'),nl, write('Enter your new guess: '), readln(Ln1), checkLine(Ln1,S, CLn).
+
+% Checks if colors in line is found.
+% Ln: List of colors.
+checkColors([],_).
+checkColors([H|T],Co) :- color(CH,H), -1< CH, CH < Co, checkColors(T).
+
+
+
+
+% accumulator initilization
+matchinit(Code, Move, Res) :- posmatch(Code, Move, [], [], [], Res).
+
 colormatch([], _, _, []).
 colormatch([_|CodeT], [], Original, Res) :- 
     colormatch(CodeT, Original, Original, Res).
@@ -8,20 +59,6 @@ colormatch([H|CodeT], [H|_], Original, [0|Res]) :-
 colormatch([H|CodeT], [X|MoveT], Original, Res) :- 
     dif(H,X), 
     colormatch([H|CodeT], MoveT, Original, Res).
-%Reads one char
-readOneChar(I) :- readln([I|_]).
-
-% init number of colors
-% C: Number of colors (output)
-initColor(C) :- write('Enter number of colors (min. 2, max. 10) '), readOneChar(A), checkColor(A,C).
-
-% accumulator initilization
-matchinit(Code, Move, Res) :- posmatch(Code, Move, [], [], [], Res).
-% check if color is out of bounds
-% A: Number of colors (input), C: Number of colors (output)
-checkColor(A,C) :- 1 < A, A < 11, C is A.
-checkColor(A,C) :- 2 > A, initColor(C).
-checkColor(A,C) :- A > 10, initColor(C).
 
 % base case
 posmatch([], [], CodeRes, MoveRes, A, Res) :- 
@@ -33,38 +70,11 @@ posmatch([H|CodeT], [H|MoveT], CodeRes, MoveRes, A, Res) :- % when color and pos
 posmatch([X|CodeT], [Y|MoveT], CodeRes, MoveRes, A, Res) :- %when color and position don't match - 
     dif(X,Y),
     posmatch(CodeT, MoveT, [X|CodeRes], [Y|MoveRes], A, Res).
-% init size of code
-% S: Size of code (output)
-initCodeSize(S) :- write('Enter size of code (min. 2, max. 10) '), readOneChar(A), checkSize(A,S).
-
-% check if size of code is out of bounds
-% A: Size of code (input), S: Size of code (output)
-checkSize(A,S) :- 1 < A, A < 11, S is A.
-checkSize(A,S) :- 2 > A, initCodeSize(S).
-checkSize(A,S) :- A > 10, initCodeSize(S).
-
-
- 
-
-% Translate S colors to integers and checks if the line has S number of colors.
-% Ln: Code in colors, C: Code in integers, Cnt: Counter starting from S going to 0, S: size of code
-colToInt([], _, 0).
-colToInt([H|T], [I|R], Cnt) :- Cnt1 is Cnt-1, color(I,H),colToInt(T, R, Cnt1). 
-
-% Parse line from colors to integers. 
-% S: Size of code, C: Code in integers
-parseLine(S,C):-write('Enter your guess: '), readln(Ln), checkLine(Ln,S,C), colToInt(Ln,C,S).
-
-% Checks if line has S colors.
-% Ln: List of colors, S: Number of colors in code, C: Code in integers.
-checkLine(Ln, S, _) :- length(Ln, S).
-checkLine(Ln, S, C) :- length(Ln, D), dif(S,D), write('You need to provide ' + S + ' colors.'), parseLine(S,C).
-
 
 
 %Guess
-% S: Size of code, C: Code in integers
-guess(S) :- parseLine(S,C), write(C).
+% S: Size of code, Co: Number of different colors, C: Code in integers
+guess(S,Co) :- parseLine(S,Co,C), write(C).
 
 
 
@@ -127,12 +137,11 @@ color(9,purple).
 
 
 
-:-run(intro)
 
 
 % Game
 % C: Number of colors S: Size of code
-gameloop(C,S) :- C1 is C-1, showColoroptions(C1), guess(S).
+gameloop(C,S) :- C1 is C-1, showColoroptions(C1), guess(S,C1).
 
 
 % Start game
